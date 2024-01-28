ee5809f4fddbc6881e4211b3228fff52a83c3da2
UdayanKhanra <115362430+uklm10@users.noreply.github.com>
2024-01-28 23:57:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (10.32%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> leftSymbols;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                leftSymbols.push(c);
+            } else if (!leftSymbols.empty() && 
+                       ((c == ')' && leftSymbols.top() == '(') ||
+                        (c == '}' && leftSymbols.top() == '{') ||
+                        (c == ']' && leftSymbols.top() == '['))) {
+                leftSymbols.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return leftSymbols.empty();
+    }
+};
