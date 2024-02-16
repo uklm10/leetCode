5892c5e38ff24773266bbfa5a8d36eb53aa21675
UdayanKhanra <115362430+uklm10@users.noreply.github.com>
2024-02-16 23:39:14 +0530

    Time: 62 ms (44.76%) | Memory: 51.7 MB (19.27%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let currentValue = init;
+
+  return {
+    increment() {
+      currentValue++;
+      return currentValue;
+    },
+    decrement() {
+      currentValue--;
+      return currentValue;
+    },
+    reset() {
+      currentValue = init;
+      return currentValue;
+    },
+  };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
