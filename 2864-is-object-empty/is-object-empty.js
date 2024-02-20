74b2d917b535627ae0f2c42fde17b7572bf59f27
UdayanKhanra <115362430+uklm10@users.noreply.github.com>
2024-02-20 23:20:51 +0530

    Time: 59 ms (23.11%) | Memory: 49.1 MB (39.78%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -3,8 +3,19 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    for (const x in obj){
-        return false;
-    }
+   if (Array.isArray(obj)) {
+    return obj.length === 0;
+  }
+
+  if (obj === null || obj === undefined) {
     return true;
+  }
+
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+
+  return true;
 };
